acbdd586d42967a50f2e0a75decc9530e8d8fc60
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2016-12-23 22:20:04 +0530

    Add: Day24 solution in Java language

---
+++ Day24/Solution.java
@@ -0,0 +1,18 @@
+public static Node removeDuplicates(Node head) {
+      //Write your code here
+        Node nextNode = head.next;
+        Node current = head;
+
+      while(current.next!= null){
+
+          if (current.data == current.next.data){
+            current.next = current.next.next;
+          }
+          else
+              current = current.next;
+
+      }
+
+      return head;
+
+    }
