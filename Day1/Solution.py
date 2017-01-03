2ea76b01730fb1d17628074531bf7fb60905dddc
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-03 13:01:59 +0530

    Add: Day1 solution in Python language

---
+++ Day1/Solution.py
@@ -0,0 +1,11 @@
+# Read and save an integer, double, and String to your variables.
+j = int (raw_input())
+e = float(raw_input())
+t = raw_input()
+# Print the sum of both integer variables on a new line.
+print i+j
+# Print the sum of the double variables on a new line.
+print d+e
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s+t
