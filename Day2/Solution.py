231254db1cb7931d4eebb4de29094328c58482aa
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-10 21:32:08 +0530

    Add: Day2 Solution in Python Language

---
+++ Day2/Solution.py
@@ -0,0 +1,8 @@
+mealCost = float(raw_input())
+tip = int(raw_input())
+tax = int(raw_input())
+tip=tip*mealCost/100;
+tax=tax*mealCost/100;
+totalcost=mealCost+tip+tax;
+
+print "The total meal cost is %s dollars." %str(int(round(totalcost, 0)))
