3b1ec796939c34c395f408221ddb85e89ba98726
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-16 23:18:04 +0530

    update: code of python language corrected

--- Day4/Solution.py
+++ Day4/Solution.py
@@ -1,23 +1,21 @@
 class Person:
     def __init__(self,initialAge):
         # Add some more code to run some checks on initialAge
-        if(initialAge>0):
-            age = initialAge
+        if(initialAge > 0):
+            self.age = initialAge
         else:
             print "Age is not valid, setting age to 0."
-            age = 0
+            self.age = 0
 
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
-        if(age<13):
-            message = "You are young."
-        elif(age>=13 and age<18):
-            message = "You are a teenager."
-        else:
-            message = "You are old."
-
-        print message
+        if self.age >= 18:
+            print("You are old.")
+        elif self.age >= 13:
+            print("You are a teenager.")
+        else: # age < 13
+            print("You are young.")
 
     def yearPasses(self):
         # Increment the age of the person in here
-         age = age + 1
+        self.age += 1
