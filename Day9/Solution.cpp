23545904e7a1d956ee85669313e7da37d7dcf3e1
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-25 22:42:01 +0530

    Add: Day9 Solution in C++ Language

---
+++ Day9/Solution.cpp
@@ -0,0 +1,13 @@
+int factorial(int n){
+        if(n<=1)
+            return 1;
+        else
+            return n*factorial(n-1);
+    }
+
+int main() {
+    int n;
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
