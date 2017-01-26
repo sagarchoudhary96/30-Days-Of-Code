2e2b490cf35efea90b836bc5e9c17cd1a04129a4
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-26 22:46:08 +0530

    Add: Day10 Solution in Python Language

---
+++ Day10/Solution.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import sys
+
+def max(a,b):
+    return a if a>b else b
+
+n = int(raw_input().strip())
+
+max_num = 0
+count = 0
+
+while n:
+    while n&1:
+        count += 1
+        n>>=1
+    max_num = max(count, max_num)
+    if not n&1:
+        count = 0
+        n>>=1
+
+print max_num
