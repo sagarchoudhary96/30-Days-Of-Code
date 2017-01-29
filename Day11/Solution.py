eb1ea33cf46a225837676ddbb05cac0f89e4bfaf
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-29 23:22:53 +0530

    Add: Day11 Solution in  Python Language

---
+++ Day11/Solution.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+import sys
+
+
+arr = []
+for arr_i in xrange(6):
+   arr_temp = map(int,raw_input().strip().split(' '))
+   arr.append(arr_temp)
+max = 0
+
+for i in xrange(0,4):
+    for j in range(0,4):
+        sum = 0
+        sum= arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+        if i==0 and j==0:
+            max = sum
+        if sum > max:
+            max =sum
+
+print max
