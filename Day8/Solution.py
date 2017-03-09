b5876713929c9e9819a84f8ade0433457c240875
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-03-09 17:15:55 +0530

    Add: Day8 Solution in Python Language

---
+++ Day8/Solution.py
@@ -0,0 +1,15 @@
+n = int(raw_input())
+phonebook = {}
+for i in range(0,n):
+    name, id = raw_input().split()
+    phonebook[name] = id
+while (1):
+    temp = ''
+    name = raw_input()
+    if (name == temp):
+        break
+    temp = name
+    if(phonebook.has_key(name)):
+        print name + "=" + phonebook[name]
+    else:
+        print "Not found"
