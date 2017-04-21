2c7e5116a151bac9d42ff2dc37539dfba8e31e15
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-04-21 10:27:53 +0530

    Add: Day12 Solution in Python Language

---
+++ Day12/Solution.py
@@ -0,0 +1,24 @@
+class Student(Person):
+    def __init__(self, fName, lName, sId, scores):
+        Person.__init__(self, fName, lName, sId)
+        self.scores = scores
+
+    def calculate(self):
+        avg=0.0
+        for score in scores:
+            avg += score
+        avg = avg/len(scores)
+        if (avg<40):
+            return 'T'
+        elif (avg<55):
+            return 'D'
+        elif (avg<70):
+            return 'P'
+        elif (avg<80):
+            return 'A'
+        elif (avg<90):
+            return 'E'
+        else:
+            return 'O'
+            
+    
