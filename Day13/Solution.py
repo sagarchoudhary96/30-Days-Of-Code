291231b0f769b9447c80381d905549957287c739
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-04-22 13:41:28 +0530

    Add: Day13 Solution in Python Language

---
+++ Day13/Solution.py
@@ -0,0 +1,7 @@
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        Book.__init__(self, title, author)
+        self.price = price
+        
+    def display(self):
+        print "Title: %s\nAuthor: %s\nPrice: %s" %(title, author, price)
