c238ec94ba2b020b55e52cef185bdaf9cb0a38cc
Sagar Choudhary <sagar.choudhary96@gmail.com>
2017-05-19 22:09:00 +0530

    Add: Day24 Solution in C Language

---
+++ Day24/Solution.c
@@ -0,0 +1,13 @@
+Node* removeDuplicates(Node *head){
+    Node *current = head;
+    while(current->next != NULL){
+        if(current->data == current->next->data){
+            current->next = current->next->next;
+        }
+        else{
+            current = current->next;
+        }
+    }
+
+    return head;
+}
