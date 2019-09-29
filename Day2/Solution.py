15f3c30dc0f1115a741a509b401021adc168e608
Sagar Choudhary <sagar.choudhary96@gmail.com>
2019-09-29 23:08:28 +0530

    Fix: Issue with python Solution of Day2

--- Day2/Solution.py
+++ Day2/Solution.py
@@ -1,8 +1,8 @@
-mealCost = float(raw_input())
-tip = int(raw_input())
-tax = int(raw_input())
+mealCost = float(input())
+tip = int(input())
+tax = int(input())
 tip=tip*mealCost/100;
 tax=tax*mealCost/100;
 totalcost=mealCost+tip+tax;
 
-print ("The total meal cost is %s dollars." %str(int(round(totalcost, 0)))
+print ("The total meal cost is %s dollars." %str(int(round(totalcost, 0))))
