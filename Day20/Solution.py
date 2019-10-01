ee9bc8794a2c2dc82408e8f2daa2e12cb2a658cf
osoba <osoba@users.noreply.github.com>
2019-10-01 18:01:54 +0200

    Update: solution for [Day 20] in [Python] language to run with python v3

--- Day20/Solution.py
+++ Day20/Solution.py
@@ -3,8 +3,8 @@
 import sys
 
 
-n = int(raw_input().strip())
-a = map(int,raw_input().strip().split(' '))
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
 numberOfSwaps = 0
 for i in range(0,n):
     for j in range(0, n-1):
@@ -15,6 +15,6 @@ for i in range(0,n):
             numberOfSwaps += 1
     if (numberOfSwaps == 0):
         break
-print "Array is sorted in " + str(numberOfSwaps) + " swaps."
-print "First Element: " + str(a[0])
-print "Last Element: " +  str(a[n-1])
+print( "Array is sorted in " + str(numberOfSwaps) + " swaps." )
+print( "First Element: " + str(a[0]) )
+print( "Last Element: " +  str(a[n-1]) )
