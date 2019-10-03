a094963eac208ee9464e2d97d5ecb000bf0e422a
TImofey <kristoft.1329@gmail.com>
2019-10-03 16:18:14 +0300

    Add solution for Day 1 in Swift language

---
+++ Day1/Solution.swift
@@ -0,0 +1,18 @@
+let i = 4
+let d = 4.0
+let s = "HackerRank "
+
+/* Declare second Int, Double, and String variables */
+let j = Int(readLine()!)!
+let e = Double(readLine()!)!
+let t = readLine()!
+
+/* Print the sum of both integer variables on a new line. */
+print(i + j)
+
+/* Print he sum of the double variables on a new line. */
+print(d + e)
+
+/* Concatenate and print the String variables on a new line;
+the 's' variable above should be printed first. */
+print(s + t)
