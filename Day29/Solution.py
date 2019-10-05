d13f1a4c0c5e8154ce438ddda2e6b79c01713fbe
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-10-06 05:05:51 +1300

    Update: solution for [Day 29] in [Python] language to run with python v3

--- Day29/Solution.py
+++ Day29/Solution.py
@@ -3,8 +3,8 @@
 import sys
 
 
-t = int(raw_input().strip())
-for a0 in xrange(t):
-    n,k = raw_input().strip().split(' ')
-    n,k = [int(n),int(k)]
+t = int(input().strip())
+for a0 in range(t):
+    n, k = input().strip().split(' ')
+    n, k = [int(n), int(k)]
     print(k-1 if ((k-1) | k) <= n else k-2)
