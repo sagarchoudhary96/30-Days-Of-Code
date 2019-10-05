5bd2326c0419c7e5744ae401a77d9ba0a1e8f975
Yannik Luysberg <36898462+TheNyneR@users.noreply.github.com>
2019-10-05 18:24:39 +0200

    Update: solution for [Day 27] in [Java] language

--- Day27/Solution.java
+++ Day27/Solution.java
@@ -1,31 +1,28 @@
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        System.out.println("3");
-        System.out.println("4 3");
-        System.out.println("-1 0 1 4");
-        System.out.println("5 3");
-        System.out.println("0 2 -4 -6 9");
-        System.out.println("6 4");
-        System.out.println("1 0 -3 4 -2 7");
+    static class TestDataEmptyArray {
+        public static int[] get_array() {
+            return new int[0];
+        }
+    }
 
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            int n = sc.nextInt();
-            int k = sc.nextInt();
-            int count=0;
-            int[] a = new int[n];
-            for(int a_i = 0;a_i < n;a_i++){
-                a[a_i] = sc.nextInt();
-                if(a[a_i]<=0)
-                    count++;
-            }
-            if(count>=k)
-                System.out.println("NO");
-            else
-                System.out.println("YES");
+    static class TestDataUniqueValues {
+        public static int[] get_array() {
+            return new int[]{11, 3, 7, 2, 4, 86, 13};
+        }
+
+        public static int get_expected_result() {
+            return 3;
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+        public static int[] get_array() {
+            return new int[]{13, 12, 321, 61, 67, 34, 12};
+        }
+
+        public static int get_expected_result() {
+            return 1;
         }
     }
 }
