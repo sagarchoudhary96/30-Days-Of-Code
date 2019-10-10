9665db743c04d5c6ab931db685d1ae7c6b7e018c
narain pattabhiraman <narainp@outlook.com>
2019-10-10 23:10:16 +0530

    Update: solution for [Day 22] in [Python] language to run with python v3

--- Day22/Solution.py
+++ Day22/Solution.py
@@ -1,10 +1,5 @@
 def getHeight(self,root):
-	if not root:
-	    return -1
-	else:
-	    a = self.getHeight(root.left)
-	    b = self.getHeight(root.right)
-	    if (a>b):
-		return 1+a
-	    else:
-		return 1+b
+        if root is None or (root.left is None and root.right is None):
+            return 0
+        else:
+            return max(self.getHeight(root.left),self.getHeight(root.right))+1
