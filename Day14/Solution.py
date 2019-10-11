593e6d16f37fb0de484c750d7db89a9e2ef5bccc
narain pattabhiraman <narainp@outlook.com>
2019-10-11 08:19:03 +0530

    Update: solution for [Day 14] in [Python] language to run with python v3

--- Day14/Solution.py
+++ Day14/Solution.py
@@ -1,7 +1,7 @@
-	self.maximumDifference = 0
+class Difference:
+	def __init__(self, a):
+		self.elements=a
+		self.maximumDifference = 0
      
-    def computeDifference(self):
-        for a in self.__elements:
-            for b in self.__elements:
-                if abs(a-b) > self.maximumDifference:
-                    self.maximumDifference = abs(a-b)
+	def computeDifference(self):
+        self.maximumDifference=max(a)-min(a)
