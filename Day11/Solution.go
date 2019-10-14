df5d58755c809d52e5e823c8df8b09149e727766
vigneshashokan <vigneshgashokan@gmail.com>
2019-10-14 11:37:04 -0600

    Add: solution for [Day 11] in [Go] language

---
+++ Day11/Solution.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	var arr [][]int32
+	for i := 0; i < 6; i++ {
+		arrRowTemp := strings.Split(readLine(reader), " ")
+
+		var arrRow []int32
+		for _, arrRowItem := range arrRowTemp {
+			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arrRow = append(arrRow, arrItem)
+		}
+
+		if len(arrRow) != int(6) {
+			panic("Bad input")
+		}
+
+		arr = append(arr, arrRow)
+	}
+
+	max := int32(math.MinInt32)
+	for i := 1; i < 5; i++ {
+		for j := 1; j < 5; j++ {
+			temp := arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
+			if temp > max {
+				max = temp
+			}
+		}
+	}
+	fmt.Println(max)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
